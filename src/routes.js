98db38256859c386f54ad9a9748030fed48b932d
Jorden McDonald <hey@jordenmcdonald.com>
2022-07-25 17:47:38 -0700

    route change

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,14 @@ import React, { lazy } from "react";
 
 import { Routes, Route } from "react-router-dom";
 
-const LocationsPage = lazy(() => import("./components/Locations/Locations"));
+// const LocationsPage = lazy(() => import("./components/Locations/Locations"));
 const HomePage = lazy(() => import("./components/Home/Home"));
 
 function appRoutes() {
   return (
     <Routes>
-      <Route key={"locations"} path={"/"} element={<LocationsPage />} />
-      <Route key={"home"} path={"/home"} element={<HomePage />} />
+      {/* <Route key={"locations"} path={"/"} element={<LocationsPage />} /> */}
+      <Route key={"home"} path={"/"} element={<HomePage />} />
     </Routes>
   );
 }
